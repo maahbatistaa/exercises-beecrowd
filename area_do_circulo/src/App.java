b19505ce430f34d20dfcbbf9d73dcbddef580ad1
Mariana Batista <mariana-batista99@hotmail.com>
2023-11-05 21:43:13 -0300

    :construction:

---
+++ area_do_circulo/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double raio , area;
+        double n = 3.14159;
+
+        raio = sc.nextDouble();
+
+        area = n * Math.pow(raio, 2);
+
+        System.out.println("Area = " + String.format("%.4f", area));
+
+        sc.close();
+    }
+}
