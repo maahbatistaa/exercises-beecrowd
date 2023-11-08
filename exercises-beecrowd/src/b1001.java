864ee277e092b45848d3041b5668d8cc78ed31b3
Mariana Batista <mariana-batista99@hotmail.com>
2023-11-08 10:13:00 -0300

    :construction:

---
+++ exercises-beecrowd/src/b1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class b1001 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int A, B, X;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        X = A + B;
+
+        System.out.println("X = " + X);
+        sc.close();
+    }
+}
