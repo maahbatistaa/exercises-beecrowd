235a872a4881f1b9a681588963aa4c16be953c35
Mariana Batista <mariana-batista99@hotmail.com>
2023-11-23 15:30:57 -0300

    :sparkles:

--- Iniciantes/b1005.js
+++ Iniciantes/b1005.js
@@ -1,4 +1,4 @@
-var input = require('fs').readFileSync('../stdin', 'utf8');
+var input = require('fs').readFileSync('./stdin', 'utf8');
 var lines = input.split('\n');
 
 var nota1 = parseFloat(lines.shift())
