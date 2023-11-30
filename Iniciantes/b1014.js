413544843f686a45a6c0618dc0515abae7cdc6a5
Mariana Batista <mariana-batista99@hotmail.com>
2023-11-30 17:11:51 -0300

    :sparkles:

--- Iniciantes/b1014.js
+++ Iniciantes/b1014.js
@@ -1,3 +1,8 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
+let X = parseInt(lines.shift())
+let Y = parseFloat(lines.shift())
+let consumo = X / Y;
+
+console.log(consumo.toFixed(3), "km/l")
