275f378106e68f0c6bdf0f2418d731ef00aefd1c
Mariana Batista <mariana-batista99@hotmail.com>
2023-11-30 17:46:45 -0300

    :sparkles:

---
+++ Iniciantes/b1016.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+let km = parseInt(lines.shift())
+let minutos = (60 * km) / 30
+
+console.log(minutos, 'minutos')
